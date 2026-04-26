0b88d7616feec322799b31f45e8f2c1edab2ea16
bhupen1005 <bhupen1005@gmail.com>
2026-04-26 11:29:48 +0530

    changed title and author

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,9 +1,9 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
+  title: 'Visual TechBlog',
+  author: 'Aditi Deshmukh',
   headerTitle: 'Visual TechBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  description: 'Learn concepts visually..',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
